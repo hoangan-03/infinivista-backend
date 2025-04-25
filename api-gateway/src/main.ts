f7f0fe467a9e632e810e209ee35101e379c64c15
An Nguyen <john12052003@gmail.com>
2025-04-25 12:41:05 +0700

    Update main.ts

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
     app.use(cookieParser(sessionSecret));
     app.use(compression());
     app.enableCors({
-        origin: 'localhost:3000',
+        origin: 'http://localhost:3000',
         allowedHeaders: ['Content-Type', 'Authorization', 'withCredentials'],
         credentials: true,
     });
